82745b16b5e0f1cf751a2ed1de3b23a12a7a5f1a
artim-anton <1Rimola0>
2022-05-21 14:15:38 +0300

    ViewBinding and Animation(Splash Screen)

--- app/src/main/java/com/artimanton/favdish/SplashActivity.kt
+++ app/src/main/java/com/artimanton/favdish/SplashActivity.kt
@@ -1,11 +1,54 @@
 package com.artimanton.favdish
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.artimanton.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(LayoutInflater.from(this))
+        setContentView(splashBinding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // "Add the code that you want to execute when animation starts")
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                // "Add the code that you want to execute when animation repeats")
+            }
+        })
     }
 }
